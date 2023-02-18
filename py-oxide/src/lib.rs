b1889b8f680e50dc87b8c2d97406f98fd070446e
yngtdd <yngtdd@gmail.com>
2023-02-17 21:51:17 -0500

    docs: Add docstring

--- py-oxide/src/lib.rs
+++ py-oxide/src/lib.rs
@@ -2,6 +2,9 @@ use pyo3::prelude::*;
 use oxide::add;
 
 /// Formats the sum of two numbers as string.
+///
+/// This uses our core library, `oxide` to handle the
+/// addition in Rust.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok(add(a, b).to_string())
