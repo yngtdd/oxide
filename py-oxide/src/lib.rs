404e7573a64d1885eedc20b6097faa25da1d490f
yngtdd <yngtdd@gmail.com>
2023-02-24 21:09:26 -0500

    feat: Add Weibull model

--- py-oxide/src/lib.rs
+++ py-oxide/src/lib.rs
@@ -1,42 +1,31 @@
 use pyo3::prelude::*;
-use oxide::{add, subtract};
+extern crate oxide as oxide_rs;
+use oxide_rs::weibull;
 
-/// Formats the sum of two numbers as string.
-///
-/// This uses our core library, `oxide` to handle the
-/// addition in Rust.
-///
-/// # Args
-///     a (usize): left operand
-///     b (usize): right operand
-///
-/// # Returns
-///     Sum of `a` and `b` as a String
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok(add(a, b).to_string())
+#[pyclass]
+struct WeibullModel {
+    shape: f64,
+    scale: f64,
+    reliability: Vec<f64>,
 }
 
-/// Formats the difference of two numbers as string.
-///
-/// This uses our core library, `oxide` to handle the
-/// addition in Rust.
-///
-/// # Args
-///     a (usize): left operand
-///     b (usize): right operand
-///
-/// # Returns
-///     Difference of `a` and `b` as a String
-#[pyfunction]
-fn subtract_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok(subtract(a, b).to_string())
+#[pymethods]
+impl WeibullModel {
+    #[new]
+    fn new(shape: f64, scale: f64, num_steps: u32) -> Self {
+        let weibull = weibull::WeibullModel::new(shape, scale);
+        let reliability = weibull::reliability(weibull.weibull, num_steps);
+
+        Self {
+            shape,
+            scale,
+            reliability,
+        }
+    }
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn oxide(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(subtract_as_string, m)?)?;
+    m.add_class::<WeibullModel>()?;
     Ok(())
 }
