a6c8953669e8f93f90367ec0e2684d7b263a1335
yngtdd <yngtdd@gmail.com>
2023-03-03 12:04:36 -0500

    feat: Add s3 upload

--- oxide-lambda/src/main.rs
+++ oxide-lambda/src/main.rs
@@ -1,3 +1,8 @@
+use log::info;
+use std::time::Duration;
+use aws_sdk_s3::Client;
+use aws_sdk_s3::presigning::config::PresigningConfig;
+use aws_config::meta::region::RegionProviderChain;
 use lambda_runtime::{service_fn, Error, LambdaEvent};
 use serde::{Deserialize, Serialize};
 use statrs::distribution::Weibull;
@@ -23,14 +28,51 @@ fn draw_reliability(shape: f64, scale: f64) -> Vec<f64> {
     reliability
 }
 
+/// Save the reliability curve to S3
+async fn save_reliability(
+    client: &Client,
+    bucket: &str,
+    reliability: &[f64],
+    filename: &str,
+    expires_in: u64,
+) -> Result<(), Error> {
+    let expires_in = Duration::from_secs(expires_in);
+    let serialized_data = bincode::serialize(reliability)?;
+
+    let presigned_request = client
+        .put_object()
+        .bucket(bucket)
+        .key(filename)
+        .body(serialized_data.into())
+        .presigned(PresigningConfig::expires_in(expires_in)?)
+        .await?;
+
+    println!("Object URI: {}", presigned_request.uri());
+
+    Ok(())
+}
+
 async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
+    info!("Handling a reliability request...");
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&config);
+    let bucket = std::env::var("BUCKET_NAME")
+        .expect("A BUCKET_NAME must be set in this app's Lambda environment variables.");
+
+    info!("Draw reliability curve from Weibull");
     let shape = event.payload.weibull_shape;
     let scale = event.payload.weibull_scale;
     let reliability = draw_reliability(shape, scale);
+    let num_points = reliability.len();
+    let filename = event.payload.asset_id;
+
+    save_reliability(&client, &bucket, &reliability, &filename, 900).await?;
+    info!("Saved reliability to S3");
 
     let resp = Response {
         req_id: event.context.request_id,
-        msg: format!("Asset reliability curve has {} timesteps!", reliability.len()),
+        msg: format!("Asset reliability curve has {} timesteps!", num_points),
     };
 
     Ok(resp)
